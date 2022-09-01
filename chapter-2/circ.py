d9199d83c95f21af25517a4da073c4c11333c868
JamesChen39 <104873100+jameschen2004@users.noreply.github.com>
2022-09-01 05:14:58 +0000

    Added chapter 2 answers

---
+++ chapter-2/circ.py
@@ -0,0 +1,10 @@
+# Fall 2022 - 127 - Work
+# Name: James Chen
+
+# GitHub username: jameschen2004
+
+# this is my solution to exercise 2.8
+
+radius = float(input("Please enter the radius of your circle."))
+pi = 3.14159265359
+print("The area of your circle is", radius * pi)
