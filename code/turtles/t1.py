f3e2e476ee931f7d78307a8d73f559d2123b1acb
JamesChen39 <104873100+jameschen2004@users.noreply.github.com>
2022-09-01 13:42:13 +0000

    added turtles

---
+++ code/turtles/t1.py
@@ -0,0 +1,9 @@
+import turtle
+wn = turtle.Screen()
+crush = turtle.Turtle()
+
+for i in [0,1,2,3]:
+  crush.forward(50)
+  crush.right(90)
+
+wn.exitonclick()
---
+++ code/turtles/t2.py
@@ -0,0 +1,9 @@
+import turtle
+wn = turtle.Screen()
+squirt = turtle.Turtle()
+
+for i in [0,1,2]:
+  squirt.forward(50)
+  squirt.right(120)
+  
+wn.exitonclick()
