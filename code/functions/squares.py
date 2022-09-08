68c03dc687fb2be09291d05a58a142abd5b5e6b6
JamesChen39 <104873100+jameschen2004@users.noreply.github.com>
2022-09-08 13:32:26 +0000

    added ngon function

--- code/functions/squares.py
+++ code/functions/squares.py
@@ -2,13 +2,29 @@ import turtle
 crush = turtle.Turtle()
 apple = turtle.Turtle()
 apple.color("red")
-def square(qwerty):
- for i in range(4):
-   qwerty.forward(50)
-   qwerty.right(90)
+# docstring
+"""
+Draw a square using the turtle passed into qwerty
+"""
+def square(qwerty,x,y,w,color,heading):
+   qwerty.penup()
+   qwerty.goto(x,y)
+   qwerty.color(color)
+   qwerty.pendown()
+   for i in range(4):
+     qwerty.forward(w)
+     qwerty.left(90)
 
 wn = turtle.Screen()
 
-square(apple)
-square(crush)
+def ngon(qwerty,x,y,sides,w,color,heading):
+   qwerty.penup()
+   qwerty.goto(x,y)
+   qwerty.color(color)
+   qwerty.pendown()
+   for i in range(sides):
+     qwerty.forward(w)
+     qwerty.left(360/sides)
+
+ngon(apple,-50,-50,8,40,"blue",0)
 wn.exitonclick()
