4ae87c93077ee44042e71c6686dc28137b49ba91
james <jameschen200465@gmail.com>
2022-10-19 17:05:29 -0400

    started madlibs

---
+++ project-01-madlibs/madlib_library.py
@@ -0,0 +1,9 @@
+# This is a library of words and phrases that will be inserted into the blanks for the madlibs project
+
+import random
+
+CARTOON_CHAR = ["Spongebob", "Timmy Turner", "Elsa", "Charlie Brown", "Yogi Bear", 
+"Shaggy", "Garfield", "Sonic", "Starfire", "Eric Cartman"]
+PROPER_NOUNS = []
+NOUNS = []
+VERBS = []
---
+++ project-01-madlibs/madlibs.py
@@ -0,0 +1,2 @@
+import madlib_library
+
