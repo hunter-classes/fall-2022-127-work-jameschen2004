12fe9e472da4712eda56e798415a1dc49418466a
james <jameschen200465@gmail.com>
2022-10-20 20:47:51 -0400

    edited madlibs

--- project-01-madlibs/madlib_library.py
+++ project-01-madlibs/madlib_library.py
@@ -4,6 +4,7 @@ import random
 
 CARTOON_CHAR = ["Spongebob", "Timmy Turner", "Elsa", "Charlie Brown", "Yogi Bear", 
 "Shaggy", "Garfield", "Sonic", "Starfire", "Eric Cartman"]
-PROPER_NOUNS = []
-NOUNS = []
-VERBS = []
+PROPER_NOUNS = ["Apple", "Samsung", "Patrick", "Wanda", "Anna", "Snoopy", "Scooby Doo", ""]
+NOUNS = ["song", random.choice(PROPER_NOUNS), "movie", "dog", "cat", "apple", "banana"]
+VERBS = ["toyed", "sung", "danced", "screamed", "laughed", "ate"]
+SENTENCE_STRUCTURE = "The <CARTOON_CHAR> <VERB> at the <NOUN>, while the <NOUN> <VERB> at the <NOUN>. "
--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,2 +1,17 @@
-import madlib_library
+# Extras that I have done are
 
+import madlib_library
+import random
+
+CARTOON_CHAR = random.choice(madlib_library.CARTOON_CHAR)
+sentence_structure = madlib_library.SENTENCE_STRUCTURE.split()
+def replace_nouns(sentence_structure):
+    final_sentence_structure = []
+    for noun in sentence_structure:
+        if noun == "<NOUN>":
+            final_sentence_structure.append(random.choice(madlib_library.NOUNS)) 
+    return final_sentence_structure
+
+print(replace_nouns(sentence_structure))
+print(random.choice(madlib_library.NOUNS))
+print(sentence_structure)
