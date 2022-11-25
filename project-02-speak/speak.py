6b1378f1e95e10e4bb16c8b642803950a91ab2e8
James <jameschen200465@gmail.com>
2022-11-25 01:52:57 -0500

    finished pirate speak project

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -11,13 +11,11 @@ The file should have lines in the form “word:translation."
 words rather than straight substitutions or inserting pirate phrases
 at appropriate points in your document.
 """
-
+import re
 pirate_dict = {}
 unshakespearify_dict = {}
 story_file = open("input.txt")
-story2 = story_file
-
-proper_nouns = ["Jack Sparrow","Montague"]
+capitalize_nouns = ["jack sparrow","romeo","i'll","montague","capulet"]
 punctuation = [".",",","?","!"]
 
 with open("pirate.dat") as file:
@@ -29,14 +27,15 @@ with open("unshakespearify.dat") as file:
   for line in file:
     (key,value) = line.split(":")
     unshakespearify_dict[(key)] = value.strip()
-
 story = []
+story2 = []
 storyline = story_file.read().split()
+storyline2 = storyline
 for word in storyline:
   include = False
   if word[-1] in punctuation:
     for key in pirate_dict.keys():
-      if key in word:
+      if key == word[:-2]:
         include = True
         story.append(pirate_dict[key] + word[-1])
     if include == False:
@@ -50,10 +49,44 @@ for word in storyline:
       story.append(word)
 story = " ".join(story)
 for definition in pirate_dict.keys():
-  story = story.replace(definition,pirate_dict.get(definition))
+  if " " in definition:
+    story = story.replace(definition,pirate_dict.get(definition))
 
-print(story2)
+story = story.lower()
+rtn = re.split('([.!?:] *)', story)
+story = ''.join([i.capitalize() for i in rtn])
+for word in capitalize_nouns:
+  story = story.replace(word,word.capitalize())
+
+for word in storyline2:
+  include = False
+  if word[-1] in punctuation:
+    for key in unshakespearify_dict.keys():
+      if key == word[:-2]:
+        include = True
+        story2.append(unshakespearify_dict[key] + word[-1])
+    if include == False:
+      story2.append(word)
+  else:
+    if word in unshakespearify_dict.keys():
+      for key in unshakespearify_dict.keys():
+        if key in word:
+          story2.append(unshakespearify_dict[key])
+    else:
+      story2.append(word)
+story2 = " ".join(story2)
+story2 = story2.lower()
+
+rtn = re.split('([.!?:] *)', story2)
+story2 = ''.join([i.capitalize() for i in rtn])
+for word in capitalize_nouns:
+  story2 = story2.replace(word,word.capitalize())
+
 for definition in unshakespearify_dict.keys():
-  story2 = story2.replace(definition,unshakespearify_dict.get(definition))
+  if " " in definition:
+    story2 = story2.replace(definition,unshakespearify_dict.get(definition))
 print("Pirate Speak: \n" + story)
 
+x = input("Would you like to read the unshakespearified version? \n")
+if x in ["Yes","yes","Y","y"]:
+  print("Unshakespearified Speak: \n" + story2)
