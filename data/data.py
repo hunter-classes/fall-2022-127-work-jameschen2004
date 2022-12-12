79f4ce54772539bb609939ee03c885d0057199c4
jameschen2004 <jameschen200465@gmail.com>
2022-12-12 01:24:43 -0500

    edits to data.py

--- data/data.py
+++ data/data.py
@@ -14,8 +14,10 @@ from matplotlib import pyplot as plt
 import csv
 
 league_data_file = open("league.csv")
-league_data = pd.read_csv(league_data_file)
+league_data = pd.read_csv(league_data_file, index_col=0)
 print(league_data.astype("string"))
+print("----------")
+print("Blue Team Kills Data")
 print(league_data["blueKills"].describe())
 print("According to this dataset, the average number of kills players on the blue side get on within the first 10 minutes of a high diamond - masters game is: ")
 print(league_data["blueKills"].mean())
@@ -23,9 +25,22 @@ print("According to this dataset, the minimum number of kills players on the blu
 print(league_data["blueKills"].min())
 print("According to this dataset, the maximum number of kills players on the blue side get within the first 10 minutes of a high diamond - masters game is: ")
 print(league_data["blueKills"].max())
+print("----------")
+print("Red Team Kills Data")
+print(league_data["redKills"].describe())
+fig, (ax0, ax1) = plt.subplots(ncols= 2)
 
-pdBlueSeries = pd.Series(league_data["blueKills"], name = "Blue Team Kills")
-pdBlueSeries.plot(legend= True)
-pdRedSeries = pd.Series(league_data["redKills"], name = "Red Team Kills")
-pdRedSeries.plot(legend= True)
+ax0.hist(league_data["blueKills"], bins= 10, density= True, histtype= "bar", color= "turquoise")
+ax0.set_title("Frequency of the Kill Amounts Blue Team Obtains Within 10 Minutes")
+ax1.hist(league_data["redKills"], bins= 10, density= True, histtype= "bar", color= "pink")
+ax1.set_title("Frequency of the Kill Amounts Red Team Obtains Within 10 Minutes")
+
 plt.show()
+
+print("From these histograms, we can conclude that the kill amount frequencies on both teams are almost identical.")
+#pdBlueSeries = pd.Series(league_data["blueKills"], name = "Blue Team Kills")
+#pdBlueSeries.plot(legend= True)
+#pdRedSeries = pd.Series(league_data["redKills"], name = "Red Team Kills")
+#pdRedSeries.plot(legend= True)
+#plt.show()
+
